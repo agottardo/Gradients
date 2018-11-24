b56f3a61647eca653a78babe7d874ea0fe093dd3
Andrea Gottardo <andrea@gottardo.me>
2018-11-23 21:11:20 -0800

    Initial Commit

---
+++ Gradients/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gradients
+//
+//  Created by Andrea Gottardo on 2018-11-23.
+//  Copyright © 2018 Andrea Gottardo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
